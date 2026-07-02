13e8c9158fd016961eae373c556d660f0940c617
Kritika <155988077+kritiikkaaa@users.noreply.github.com>
2026-07-02 21:13:15 +0530

    Time: 16 ms (67.95%), Space: 32.7 MB (47.15%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -2,40 +2,25 @@ class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m = grid.size(), n = grid[0].size();
-
+        deque<pair<int,int>> dq;
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-
-        deque<pair<int, int>> dq;
-
         dist[0][0] = grid[0][0];
         dq.push_front({0, 0});
-
-        int dir[5] = {-1, 0, 1, 0, -1};
-
+        int dx[4] = {1, -1, 0, 0};
+        int dy[4] = {0, 0, 1, -1};
         while (!dq.empty()) {
             auto [x, y] = dq.front();
             dq.pop_front();
-
             for (int k = 0; k < 4; k++) {
-                int nx = x + dir[k];
-                int ny = y + dir[k + 1];
-
-                if (nx < 0 || ny < 0 || nx >= m || ny >= n)
-                    continue;
-
-                int newCost = dist[x][y] + grid[nx][ny];
-
-                if (newCost < dist[nx][ny]) {
-                    dist[nx][ny] = newCost;
-
-                    if (grid[nx][ny] == 0)
-                        dq.push_front({nx, ny});
-                    else
-                        dq.push_back({nx, ny});
+                int nx = x + dx[k], ny = y + dy[k];
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+                int w = grid[nx][ny];
+                if (dist[x][y] + w < dist[nx][ny]) {
+                    dist[nx][ny] = dist[x][y] + w;
+                    w == 0 ? dq.push_front({nx, ny}) : dq.push_back({nx, ny});
                 }
             }
         }
-
         return dist[m - 1][n - 1] < health;
     }
 };
