fdd6b09a731c4a3a57df30b40dc42a84b97ecffa
kritika <155988077+kritiikkaaa@users.noreply.github.com>
2026-07-16 20:07:43 +0530

    Time: 275 ms (94.69%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,6 +1,3 @@
-SELECT x, y, z, 
-        CASE 
-            WHEN x + y > z AND y + z > x AND x + z > y
-            THEN 'Yes' ELSE 'No'
-            END AS triangle
+SELECT *, 
+    IF(x + y > z AND y + z > x AND  x + z > y, "Yes", "No") AS triangle 
 FROM Triangle;
