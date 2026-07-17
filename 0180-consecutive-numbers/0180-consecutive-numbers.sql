402a6a5ed15333308f7f9a254e3603bad73cbc05
kritika <155988077+kritiikkaaa@users.noreply.github.com>
2026-07-17 19:33:36 +0530

    Time: 545 ms (84.69%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+WHERE EXISTS (
+    SELECT 1
+    FROM Logs l2
+    WHERE l2.id = l1.id + 1 AND l2.num = l1.num
+    AND EXISTS (
+        SELECT 1
+        FROM Logs l3
+        WHERE l3.id = l1.id + 2 AND l3.num = l1.num
+    )
+);
